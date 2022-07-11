4ae6d9239babf658fc2fce6b968c524866ec33a2
Neil Antrassian <neilantrassian@Neils-MacBook-Pro.local>
2022-07-11 18:42:37 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='Homework 14 package',
+      url='https://github.com/neilantrassian/Books',
+      author='Neil Antrassian',
+      author_email='mst8gd@virginia.edu',
+      license='MIT',
+      packages=['book_demo'])
